4553c966d6703442bfff3bedc1183366053a2d4e
Santosh Marigowda <santosharakere@gmail.com>
2019-06-07 05:10:15 +0100

    object literal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello, Welcome to OOP in JS");
+
+const circle = {
+    radius: 1,
+    x: 10,
+    y: 20,
+    draw: () => {
+        console.log('draw method called...');
+    }
+}
+
+circle.draw();
