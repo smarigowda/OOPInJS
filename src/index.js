eca77e9229610037f9cee41041ecbb83caac9ea8
Santosh Marigowda <santosharakere@gmail.com>
2019-06-07 21:35:23 +0100

    calling function using apply method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // constructor function
-function Circle(radius) {
-    console.log(this);
-    this.radius = radius;
-    this.draw = () => {
-        console.log(`Draw a circle with radius: ${radius}`);
-    }
+function Circle(radius, x, y) {
+  console.log(this);
+  console.log(`radius: ${radius}, x: ${x} and y: ${y}`);
+  this.radius = radius;
+  this.draw = () => {
+    console.log(`Draw a circle with radius = ${radius}, x = ${x} and y = ${y}`);
+  };
 }
-Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
