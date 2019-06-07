63592142b11f716f2b28a6cfb0d05dedf4032908
Santosh Marigowda <santosharakere@gmail.com>
2019-06-07 05:18:44 +0100

    creating objects using factory function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 console.log("Hello, Welcome to OOP in JS");
 
-const circle = {
-    radius: 1,
+// factory
+const createCircle = radius => {
+  return {
+    radius,
     x: 10,
     y: 20,
     draw: () => {
-        console.log('draw method called...');
+      console.log(`draw a circle with radius = ${radius}`);
     }
-}
+  };
+};
 
-circle.draw();
+const circle1 = createCircle(1);
+const circle2 = createCircle(2);
+circle1.draw();
+circle2.draw();
