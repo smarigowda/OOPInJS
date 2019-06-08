02695235617b4b6955659ccf70f6aa59c4d61604
Santosh Marigowda <santosharakere@gmail.com>
2019-06-08 20:26:06 +0100

    demo of prototype members

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ function Circle() {
   console.log(this);
   // Instance members
   this.raduis = 1.0;
-  this.draw = () => {
-    console.log('complex code to draw a circle');
-  }
 }
 
+// Prototype members
+Circle.prototype.draw = () => {
+  console.log('complex code to draw a circle');
+}
 const c1 = new Circle();
 const c2 = new Circle();
+
+c1.draw();
+c2.draw();
