657861325a2ec81194f18d366805a27931dba87e
Santosh Marigowda <santosharakere@gmail.com>
2019-06-08 04:50:06 +0100

    adding properties after object creation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 // constructor function
-function Circle(radius, x, y) {
+function Circle(radius) {
   console.log(this);
-  console.log(`radius: ${radius}, x: ${x} and y: ${y}`);
+  console.log(`radius: ${radius}`);
   this.radius = radius;
   this.draw = () => {
-    console.log(`Draw a circle with radius = ${radius}, x = ${x} and y = ${y}`);
+    console.log(`Draw a circle with radius = ${radius}`);
   };
 }
-Circle.apply({}, [1, 2, 3]);
+
+const circle1 = new Circle(25);
+
+circle1.locationX = { x: 10 };
+circle1.locationY = { y: 20 };
+const locationz='locationZ';
+
+circle1[locationz] = { z: 30 }
+
+
