d1412ad69b567dfe74e0c7a258f4d14e4437d77c
Santosh Marigowda <santosharakere@gmail.com>
2019-06-08 05:37:06 +0100

    * hiding details (abstraction) using private properties

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // constructor function
 function Circle(radius) {
+  // private
+  const defaultLocation = { x: 0, y: 0 };
+  // private
+  const computeOptimumLocation = radius => {
+    console.log("-- logic to compute optimum location for radius", radius);
+  };
   console.log(this);
   console.log(`radius: ${radius}`);
   this.radius = radius;
   this.draw = () => {
+    console.log(defaultLocation);
+    computeOptimumLocation(this.radius);
     console.log(`Draw a circle with radius = ${radius}`);
   };
 }
@@ -12,22 +20,24 @@ const circle1 = new Circle(25);
 
 circle1.locationX = { x: 10 };
 circle1.locationY = { y: 20 };
-const locationz='locationZ';
+const locationz = "locationZ";
 
-circle1[locationz] = { z: 30 }
+circle1[locationz] = { z: 30 };
 
 // delete circle1.locationX;
 
-for(let key in circle1) {
-    console.log(key);
-    if(typeof circle1[key] === 'function') {
-        console.log(`-- ${key} is a function`);
-    }
+for (let key in circle1) {
+  console.log(key);
+  if (typeof circle1[key] === "function") {
+    console.log(`-- ${key} is a function`);
+  }
 }
 
 let allKeys = Object.keys(circle1);
 console.log(allKeys);
 
-if('radius' in circle1) {
-    console.log('radius exists in circle1');
+if ("radius" in circle1) {
+  console.log("radius exists in circle1");
 }
+
+circle1.draw();
