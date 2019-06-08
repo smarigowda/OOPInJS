50b4295ca9187345a65c8ae2b03911cfa2f4a4b2
Santosh Marigowda <santosharakere@gmail.com>
2019-06-08 20:23:36 +0100

    instance members

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@
 // prototypes and inheritance
 
 function Circle() {
-  console.log("this is a circle");
   console.log(this);
+  // Instance members
+  this.raduis = 1.0;
+  this.draw = () => {
+    console.log('complex code to draw a circle');
+  }
 }
 
-const circle = new Circle();
-console.log('--- Circle ---');
-console.log('circle.__proto__:', circle.__proto__);
-console.log('Circle.prototype:', Circle.prototype);
-console.log('3:', circle.__proto__ === Circle.prototype);
-
-const square = function () {
-  console.log('This is a square');
-}
-
-console.log('--- Anonymous Function ---');
-console.log('Object.getPrototypeOf(square):', Object.getPrototypeOf(square));
-
-let x = {}; //?
-let y = {}; //?
-let ofx = Object.getPrototypeOf(x);
-let ofy = Object.getPrototypeOf(y);
-ofx === ofy;
-ofx.__proto__ === ofy.__proto__; //?
+const c1 = new Circle();
+const c2 = new Circle();
