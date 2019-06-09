eac8fa8c25873b159d02b33ec8860a6e4248a86d
Santosh Marigowda <santosharakere@gmail.com>
2019-06-09 07:00:58 +0100

    method overriding demo

--- src/prototypical-inheritance/lesson.js
+++ src/prototypical-inheritance/lesson.js
@@ -1,24 +1,20 @@
 function Shape(color) {
-    this.color = color;
-    this.move = function() {
-        console.log('move method...');
-    }
 }
 
 Shape.prototype.duplicate = function() {
-    console.log('duplicate method...');
+    console.log('duplicate method...Shape constructor');
 }
 
-function Circle(radius, color) {
-    Shape.call(this, color);
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw method');
-    }
+function Circle() {
 }
 
 Circle.prototype = Object.create(Shape.prototype); // Object.create sets up the prototype properlty
 Circle.prototype.constructor = Circle;
 
-const s1 = new Shape('green');
-const c1 = new Circle(1, 'red');
+Circle.prototype.duplicate = function() {
+    Shape.prototype.duplicate.call(this);
+    console.log('duplicate method...Circle constructor')
+}
+
+const c1 = new Circle();
+c1.duplicate();
