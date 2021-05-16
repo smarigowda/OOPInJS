59c3ec064cce8cf78cb150b2fd1bd5c65ddef278
Santosh Marigowda <santosh.marigowda2@sky.uk>
2021-05-16 16:12:21 +0100

    prototypical inheritance exercise

---
+++ src/prototypical-inheritance/exercise.js
@@ -0,0 +1,31 @@
+function HtmlElement() {
+  this.click = function () {
+    console.log("click");
+  };
+}
+
+HtmlElement.prototype.focus = function () {
+  console.log("focus");
+};
+
+const htmlElement = new HtmlElement();
+console.log(htmlElement);
+
+function HtmlSelectElement() {
+  this.items = [];
+  this.addItem = function (item) {
+    this.items.push(item);
+  };
+  this.removeItem = function (item) {
+    this.items = this.items.filter((data) => {
+      return data !== item;
+    });
+  };
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+const htmlSelectElement = new HtmlSelectElement();
+console.log(htmlSelectElement);
+
