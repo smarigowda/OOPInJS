b95e40aec531d4181295fd680885d0237aade8d8
Santosh Marigowda <santosh.marigowda2@sky.uk>
2021-05-16 17:22:20 +0100

    polymorphism exercise completed

--- src/prototypical-inheritance/exercise.js
+++ src/prototypical-inheritance/exercise.js
@@ -11,7 +11,7 @@ HtmlElement.prototype.focus = function () {
 const htmlElement = new HtmlElement();
 console.log(htmlElement);
 
-function HtmlSelectElement(items=[]) {
+function HtmlSelectElement(items = []) {
   this.items = items;
   this.addItem = function (item) {
     this.items.push(item);
@@ -21,11 +21,39 @@ function HtmlSelectElement(items=[]) {
       return data !== item;
     });
   };
+  this.render = function () {
+    let list = this.items.map((item) => {
+      return `   <option>${item}</option>`;
+    });
+    return `<select>\n${list.join('\n')}
+</select>`;
+  };
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
-const htmlSelectElement = new HtmlSelectElement();
+const htmlSelectElement = new HtmlSelectElement([1, 2, 3, 4]);
 console.log(htmlSelectElement);
 
+function HtmlImageElement(src) {
+  this.src = src;
+  this.render = function () {
+    return `<img src="${this.src}">`;
+  };
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const htmlImageElement = new HtmlImageElement("http://");
+console.log(htmlImageElement);
+
+const htmlElements = [
+  new HtmlSelectElement([1, 2, 3, 4]),
+  new HtmlImageElement("http://"),
+];
+
+for(let element of htmlElements) {
+  console.log(element.render());
+}
