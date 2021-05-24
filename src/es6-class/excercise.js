1e1b1e62cb40bcc1ee4a14439496aed3e9aa6613
Santosh Marigowda <santosh.marigowda2@sky.uk>
2021-05-24 06:13:15 +0100

    updating private variable

--- src/es6-class/excercise.js
+++ src/es6-class/excercise.js
@@ -1,27 +1,29 @@
 class Stack {
-    #stack = [];
-
-    push(item) {
-        this.stack.push(item);
+  #stack;
+  constructor() {
+    this.#stack = [];
+  }
+  push(item) {
+    this.#stack.push(item);
+  }
+  peek() {
+    if (this.#stack.length === 0) {
+      throw new Error("Stack is empty");
     }
-    peek() {
-        if(this.stack.length === 0) {
-            throw new Error('Stack is empty');
-        }
-        return this.stack[this.stack.length - 1];
+    return this.#stack[this.#stack.length - 1];
+  }
+  pop() {
+    if (this.#stack.length === 0) {
+      throw new Error("Stack is empty");
     }
-    pop() {
-        if(this.stack.length === 0) {
-            throw new Error('Stack is empty');
-        }
-        let countOfItems = this.stack.length;
-        let lastItem = this.stack[countOfItems - 1];
-        this.stack = this.stack.slice(0, countOfItems - 1);
-        return lastItem;
-    }
-    get count() {
-        return this.stack.length;
-    }
+    let countOfItems = this.#stack.length;
+    let lastItem = this.#stack[countOfItems - 1];
+    this.#stack = this.#stack.slice(0, countOfItems - 1);
+    return lastItem;
+  }
+  get count() {
+    return this.#stack.length;
+  }
 }
 
 const s = new Stack();
