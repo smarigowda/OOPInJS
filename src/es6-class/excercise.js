7db12a3f29fb0e2a000343e6ad576855cef0fdf0
Santosh Marigowda <santosh.marigowda2@sky.uk>
2021-05-24 06:05:09 +0100

    implementing stack using es6 class and private variable

---
+++ src/es6-class/excercise.js
@@ -0,0 +1,27 @@
+class Stack {
+    #stack = [];
+
+    push(item) {
+        this.stack.push(item);
+    }
+    peek() {
+        if(this.stack.length === 0) {
+            throw new Error('Stack is empty');
+        }
+        return this.stack[this.stack.length - 1];
+    }
+    pop() {
+        if(this.stack.length === 0) {
+            throw new Error('Stack is empty');
+        }
+        let countOfItems = this.stack.length;
+        let lastItem = this.stack[countOfItems - 1];
+        this.stack = this.stack.slice(0, countOfItems - 1);
+        return lastItem;
+    }
+    get count() {
+        return this.stack.length;
+    }
+}
+
+const s = new Stack();
